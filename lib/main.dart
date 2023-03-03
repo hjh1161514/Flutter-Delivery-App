dd648722ac303d2a9b5403e9e77544842fc3c900
hjh1161514 <hjh1161514@naver.com>
2023-03-03 15:53:53 +0900

    [FEAT] #1 - 로그인 화면 분리

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_delivery_app/common/component/custom_text_form_field.dart';
+import 'package:flutter_delivery_app/user/view/login_screen.dart';
 
 void main() {
   runApp(_App());
@@ -15,23 +16,7 @@ class _App extends StatelessWidget {
         fontFamily: 'NotoSans',
       ),
       debugShowCheckedModeBanner: false, // debug 배너 안 보이도록
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CustomTextFormField(
-              hintText: '이메일을 입력해주세요.',
-              onChanged: (String value) {  },
-            ),
-            CustomTextFormField(
-              hintText: '비밀번호를 입력해주세요.',
-              onChanged: (String value) {  },
-              obscureText: true,
-            )
-          ],
-        ),
-      )
+      home: LoginScreen(),
     );
   }
 }
