0f36dfac2b94e4870637e8e928c2f388ef72187c
hjh1161514 <hjh1161514@naver.com>
2023-03-03 13:30:56 +0900

    [CHORE] 프로젝트 초기화

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(_App());
+}
+
+class _App extends StatelessWidget {
+  const _App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( // 최상위에는 무조건 MaterialApp이 들어가야 함
+      debugShowCheckedModeBanner: false, // debug 배너 안 보이도록
+      home: Scaffold(
+        body: Container(),
+      )
+    );
+  }
+}
