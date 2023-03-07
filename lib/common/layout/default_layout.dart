c4a9241b93a72b8a364fc1b322813270ef5aeb88
hjh1161514 <hjh1161514@naver.com>
2023-03-07 16:10:42 +0900

    [FEAT] #3 - background color 추가

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -2,17 +2,19 @@ import 'package:flutter/material.dart';
 
 // 모든 뷰에 특정 기능을 적용하고 싶을 때 로직 추가
 class DefaultLayout extends StatelessWidget {
+  final Color? backgroundColor;
   final Widget child;
 
   const DefaultLayout({
     required this.child,
+    this.backgroundColor,
     Key? key
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: backgroundColor ?? Colors.white, // backgroundColor가 null이면 white로 설정
       body: child,
     );
   }
