f2d21985f2cb3cd871e18f5458f8b6c0d5d5e800
hjh1161514 <hjh1161514@naver.com>
2023-03-08 09:50:54 +0900

    [FEAT] #5 - TabBarVieww 생성

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -9,16 +9,40 @@ class RootTab extends StatefulWidget {
   State<RootTab> createState() => _RootTabState();
 }
 
-class _RootTabState extends State<RootTab> {
+class _RootTabState extends State<RootTab>
+  with SingleTickerProviderStateMixin{
+  late TabController controller; // late: 나중에 입력이 됨. initState에서 필수로 선언하기 때문에 가능. ?을 하면 계속 확인해야 하기 때문에 late가 더 좋음
+
   int index = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // length: children에 넣은 값들의 갯수. 컨트롤할 화면 갯수(홈,음식, 주문, 프로필)
+    // vsync : controller에 선언하는 state. this를 사용하기 위해 SingleTickerProviderStateMixin 사용 <- this에 에러가 없어지며 현재 class를 넣을 수 있음
+    controller = TabController(length: 4, vsync: this);
+    
+    controller.addListener(tabListener); // controller에서 변경이 될 때마다 실행
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(tabListener);
+    super.dispose();
+  }
+
+  void tabListener() {
+    setState(() {
+      // tab이 바뀔 때마다 현재 tab을 index에 넣어줌
+      index = controller.index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '코팩 딜리버리',
-      child: Center(
-        child: Text('Root Tab'),
-      ),
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: PRIMARY_COLOR,
         unselectedItemColor: BODY_TEXT_COLOR,
@@ -26,9 +50,7 @@ class _RootTabState extends State<RootTab> {
         unselectedFontSize: 10,
         type: BottomNavigationBarType.fixed, // shifting: 선택된 tap이 크게 보임
         onTap: (int index){
-          setState(() {
-            this.index = index;
-          });
+          controller.animateTo(index); // 화면 이동
         },
         currentIndex: index, // 선택된 것 지정
         items: [
@@ -48,6 +70,16 @@ class _RootTabState extends State<RootTab> {
             icon: Icon(Icons.person_outlined),
             label: '프로필',
           ),
+        ],
+      ),
+      child: TabBarView(
+        physics: NeverScrollableScrollPhysics(), // TabBarview에서 스크롤 시에 이동을 막음
+        controller: controller,
+        children: [
+          Container(child: Text('홈')),
+          Container(child: Text('음식')),
+          Container(child: Text('주문')),
+          Container(child: Text('프로필')),
         ],
       ),
     );
