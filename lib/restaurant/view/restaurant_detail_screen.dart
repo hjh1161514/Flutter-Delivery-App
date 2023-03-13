34fb3d3d8690761eb2ce0f728ba50ca47d22b1cf
hjh1161514 <hjh1161514@naver.com>
2023-03-13 17:39:34 +0900

    [FEAT] #9 - SliverList 구현

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -10,24 +10,79 @@ class RestaurantDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '떡볶이',
-      child: Column(
-        children: [
-          RestaurantCard(
-              image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
-              name: '불떡',
-              tags: ['떡볶이', '맛있음'],
-              ratingsCount: 100,
-              deliveryTime: 30,
-              deliveryFee: 3000,
-              ratings: 4.5,
-              isDetail: true,
-              detail: '맛떡',
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: ProductCard(),
-          )
+      child: CustomScrollView( // 두 개의 스크롤 뷰를 하나의 스크롤이 되는 것처럼 하기 위해 사용
+        slivers: [
+          renderTop(),
+          renderLabel(),
+          renderProducts(),
         ],
+      )
+      // Column(
+      //   children: [
+      //     RestaurantCard(
+      //         image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+      //         name: '불떡',
+      //         tags: ['떡볶이', '맛있음'],
+      //         ratingsCount: 100,
+      //         deliveryTime: 30,
+      //         deliveryFee: 3000,
+      //         ratings: 4.5,
+      //         isDetail: true,
+      //         detail: '맛떡',
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      //       child: ProductCard(),
+      //     )
+      //   ],
+      // ),
+    );
+  }
+
+  SliverPadding renderLabel() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          '메뉴',
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+
+  SliverPadding renderProducts() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: ProductCard(),
+              );
+            },
+          childCount: 10,
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter renderTop() {
+    return SliverToBoxAdapter( // 일반 위젯을 넣기 위해 사용
+      child: RestaurantCard(
+        image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+        name: '불떡',
+        tags: ['떡볶이', '맛있음'],
+        ratingsCount: 100,
+        deliveryTime: 30,
+        deliveryFee: 3000,
+        ratings: 4.5,
+        isDetail: true,
+        detail: '맛떡',
       ),
     );
   }
