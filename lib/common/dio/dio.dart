943325cc6e6b2d0b3b05eeb1ee23a47166520f70
hjh1161514 <hjh1161514@naver.com>
2023-03-16 11:28:33 +0900

    [FEAT] #11 - Dio onResponse Interceptor

--- lib/common/dio/dio.dart
+++ lib/common/dio/dio.dart
@@ -44,7 +44,14 @@ class CustomInterceptor extends Interceptor{
 
     return super.onRequest(options, handler); // 요청이 보내지는 순간
   }
+
   // 2) 응답을 받을 때
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    print('[RES] [${response.requestOptions.method}] ${response.requestOptions.uri}');
+
+    return super.onResponse(response, handler);
+  }
 
   // 3) 에러가 났을 때
   @override
