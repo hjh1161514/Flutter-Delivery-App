bb6ac1b925df7ee97dbd1fdb176f62e759fa7779
hjh1161514 <hjh1161514@naver.com>
2023-03-28 13:53:48 +0900

    [FEAT] #13 - Pagination: 데이터를 처음부터 가져오는 상황

--- lib/restaurant/provider/restaurant_provider.dart
+++ lib/restaurant/provider/restaurant_provider.dart
@@ -45,82 +45,108 @@ class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
     // true - CursorPaginationLoading() - 화면에 데이터가 모두 지워지고 가운데에서 로딩 effect
     bool forceRefetch = false,
 }) async{
-    // 5가지 가능성
-    // State의 상태 -> CursorPaginationBase를 extends하는 class가 5개
-    // [상태가]
-    // 1) CursorPagination - 정상적으로 데이터가 있는 상태
-    // 2) CursorPaginationLoading = 데이터가 로딩 중인 상태 (현재 캐시 없음) => forceRefetch = true
-    // 3) CursorPaginationError - 에러가 있는 상태
-    // 4) CursorPaginationRefetching - 첫번째 페이지부터 다시 데이터를 가져올 때
-    // 5) CursorPaginationFetchMore - 추가 데이터를 paginate 해오라는 요청을 받았을 떄
+    try{
+      // 5가지 가능성
+      // State의 상태 -> CursorPaginationBase를 extends하는 class가 5개
+      // [상태가]
+      // 1) CursorPagination - 정상적으로 데이터가 있는 상태
+      // 2) CursorPaginationLoading = 데이터가 로딩 중인 상태 (현재 캐시 없음) => forceRefetch = true
+      // 3) CursorPaginationError - 에러가 있는 상태
+      // 4) CursorPaginationRefetching - 첫번째 페이지부터 다시 데이터를 가져올 때
+      // 5) CursorPaginationFetchMore - 추가 데이터를 paginate 해오라는 요청을 받았을 떄
 
-    // =====
-    //  바로 반환하는 상황
-    // (1) hasMore = false (기존 상태에서 이미 다음 데이터가 없다는 값을 들고 있다면)
-    // (2) 로딩 중 - fetchMore = true => 앱에서 맨 아래까지 스크롤하고 더 데이터를 가져오라고 하는 상황
-        // 추가 데이터를 가져와야 하는 상황에서 paginate 함수가 다시 실행된다면
-        // 갖고 있는 20개 데이터에서 다음 데이터가 들어오기 전에 똑같은 요청을 넣으면 똑같은 20개의 데이터를 가져옴
+      // =====
+      //  바로 반환하는 상황
+      // (1) hasMore = false (기존 상태에서 이미 다음 데이터가 없다는 값을 들고 있다면)
+      // (2) 로딩 중 - fetchMore = true => 앱에서 맨 아래까지 스크롤하고 더 데이터를 가져오라고 하는 상황
+      // 추가 데이터를 가져와야 하는 상황에서 paginate 함수가 다시 실행된다면
+      // 갖고 있는 20개 데이터에서 다음 데이터가 들어오기 전에 똑같은 요청을 넣으면 똑같은 20개의 데이터를 가져옴
 
-    // (1)
-    // 로딩중일 때 fetchMore가 아닐 때는 실행. -> 기존 요청을 멈추고 새로운 데이터를 요청하는 거라고 생각 => 새로 고침의 의도가 있을 수 있음
-    if(state is CursorPagination && !forceRefetch) { // pagination을 한 번 이상해서 이미 데이터가 있음
-      // 아직 dart에서 state.을 바로 사용할 수 없어서 CursorPagination이라는 것을 한 번 더 명시
-      final pState = state as CursorPagination;
+      // (1)
+      // 로딩중일 때 fetchMore가 아닐 때는 실행. -> 기존 요청을 멈추고 새로운 데이터를 요청하는 거라고 생각 => 새로 고침의 의도가 있을 수 있음
+      if(state is CursorPagination && !forceRefetch) { // pagination을 한 번 이상해서 이미 데이터가 있음
+        // 아직 dart에서 state.을 바로 사용할 수 없어서 CursorPagination이라는 것을 한 번 더 명시
+        final pState = state as CursorPagination;
 
-      if (!pState.meta.hasMore) { // 더 데이터가 없으면
+        if (!pState.meta.hasMore) { // 더 데이터가 없으면
+          return;
+        }
+      }
+
+      // (2)
+      final isLoading = state is CursorPaginationLoading;
+      final isRefetching = state is CursorPaginationRefetching;
+      final isFetchingMore = state is CursorPaginationFetchingMore;
+
+      if(fetchMore && (isLoading || isRefetching || isFetchingMore)) {
         return;
       }
-    }
 
-    // (2)
-    final isLoading = state is CursorPaginationLoading;
-    final isRefetching = state is CursorPaginationRefetching;
-    final isFetchingMore = state is CursorPaginationFetchingMore;
+      // =====
+      // 아무런 값이 없으면 서버에서 오는 값(20)으로 지정
+      // PaginationParams 생성
+      PaginationParams paginationParams = PaginationParams(
+        count: fetchCount,
+      );
 
-    if(fetchMore && (isLoading || isRefetching || isFetchingMore)) {
-      return;
-    }
+      // fetchMore 상황
+      // 데이터를 추가로 더 가져오는 상황
+      if (fetchMore) {
+        final pState = state as CursorPagination; // fetchMore을 실행할 수 있는 상황 = 화면에 데이터가 보여지는 상황
 
-    // =====
-    // 아무런 값이 없으면 서버에서 오는 값(20)으로 지정
-    // PaginationParams 생성
-    PaginationParams paginationParams = PaginationParams(
-      count: fetchCount,
-    );
-
-    // fetchMore 상황
-    // 데이터를 추가로 더 가져오는 상황
-    if (fetchMore) {
-      final pState = state as CursorPagination; // fetchMore을 실행할 수 있는 상황 = 화면에 데이터가 보여지는 상황
-
-      // 데이터를 유지한 채로 class 이름만 변경
-      state = CursorPaginationFetchingMore(
+        // 데이터를 유지한 채로 class 이름만 변경
+        state = CursorPaginationFetchingMore(
           meta: pState.meta,
           data: pState.data,
+        );
+
+        paginationParams = paginationParams.copyWith(
+          after: pState.data.last.id,
+        );
+      }
+      // ===
+      // 데이터를 처음부터 가져오는 상황 - params 변경 필요 없음
+      else {
+        // 만약에 데이터가 있는 상황이라면
+        // 기존 데이터를 보존한 채로 Fetch (API 요청)를 진행
+        if (state is CursorPagination && !forceRefetch) {
+          final pState = state as CursorPagination;
+
+          // 데이터를 유지하다가 새로운 데이터가 오면 대체
+          state = CursorPaginationRefetching(
+            meta: pState.meta,
+            data: pState.data,
+          );
+        }
+        // 나머지 상황
+        else {
+          state = CursorPaginationLoading();
+        }
+      }
+
+      // 로딩이 돌아가는 순간 요청을 넣어야 함
+      // 가장 최근 데이터 (20)개
+      final resp = await repository.paginate(
+        paginationParams: paginationParams,
       );
 
-      paginationParams = paginationParams.copyWith(
-        after: pState.data.last.id,
-      );
-    }
+      if (state is CursorPaginationFetchingMore) {
+        final pState = state as CursorPaginationFetchingMore;
 
-    // 로딩이 돌아가는 순간 요청을 넣어야 함
-    // 가장 최근 데이터 (20)개
-    final resp = await repository.paginate(
-      paginationParams: paginationParams,
-    );
-
-    if (state is CursorPaginationFetchingMore) {
-      final pState = state as CursorPaginationFetchingMore;
-
-      // 응답이 다 왔기 때문에 로딩 상태를 로딩이 끝난 상태로 변경
-      state = resp.copyWith(
-        // 기존 데이터에 새로운 데이터 추가
-        data: [
-          ...pState.data, // 기존
-          ...resp.data, // 새로운
-        ]
-      );
+        // 응답이 다 왔기 때문에 로딩 상태를 로딩이 끝난 상태로 변경
+        state = resp.copyWith(
+          // 기존 데이터에 새로운 데이터 추가
+            data: [
+              ...pState.data, // 기존
+              ...resp.data, // 새로운
+            ]
+        );
+      } else {
+        // resp는 맨처음 데이터라서
+        state = resp;
+      }
+    }catch(e) {
+      state = CursorPaginationError(message: "데이터를 가져오지 못했습니다.");
     }
   }
 }
