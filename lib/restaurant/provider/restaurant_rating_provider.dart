4924e16a2b17f5f28ccf60524a650429d2af60e6
hjh1161514 <hjh1161514@naver.com>
2023-03-29 15:04:24 +0900

    [FEAT] #17 - RestaurantRatingProvider 초기작업

---
+++ lib/restaurant/provider/restaurant_rating_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_delivery_app/common/model/cursor_pagination_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../repository/restaurant_rating_repository.dart';
+
+class RestaurantRatingStateNotifier extends StateNotifier<CursorPaginationBase> {
+  final RestaurantRatingRepository repository;
+
+  RestaurantRatingStateNotifier({
+    required this.repository,
+  }):
+      super(
+        CursorPaginationLoading(),
+      );
+}
--- lib/restaurant/repository/restaurant_rating_repository.dart
+++ lib/restaurant/repository/restaurant_rating_repository.dart
@@ -1,11 +1,21 @@
 import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_delivery_app/common/dio/dio.dart';
 import 'package:flutter_delivery_app/rating/model/rating_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:retrofit/http.dart';
 
+import '../../common/const/data.dart';
 import '../../common/model/cursor_pagination_model.dart';
 import '../../common/model/pagination_params.dart';
 
 part 'restaurant_rating_repository.g.dart';
+
+// 어떤 레스토랑에서 지정하는지 알아야 하기 때문에 family로 지정
+final restaurantRatingRepositoryProvider = Provider.family<RestaurantRatingRepository, String> ((ref, id) {
+  final dio = ref.watch(dioProvider);
+  
+  return RestaurantRatingRepository(dio, baseUrl: 'http://$ip/restaurant/$id/rating');
+});
 
 @RestApi()
 abstract class RestaurantRatingRepository{
